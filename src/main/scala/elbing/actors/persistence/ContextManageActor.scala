7314ad8dcc02299c0708553581c3b023db0d397b
hxx0215 <hxx0215@126.com>
2023-08-19 09:58:07 -0400

    [Chore] remove useless var

--- src/main/scala/elbing/actors/persistence/ContextManageActor.scala
+++ src/main/scala/elbing/actors/persistence/ContextManageActor.scala
@@ -47,7 +47,7 @@ class ContextManageActor(context: ActorContext[Command]) extends AbstractBehavio
     case Update(id, topicName, value, replyTo) =>
       val actor = sharding.entityRefFor(ContextActor.TypeKey, id)
       context.ask[ContextActor.Command, ContextActor.UpdateResponse](actor, reply => ContextActor.Update(topicName, value, reply)) {
-        case Failure(exception) => CommandResult(false)
+        case Failure(_) => CommandResult(false)
         case Success(value) =>
           replyTo ! Updated(value.version)
           CommandResult(true)
