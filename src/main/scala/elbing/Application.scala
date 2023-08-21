d8cd36ca841f28042887a5f06242cf571ac27882
hxx0215 <hxx0215@126.com>
2023-08-21 11:49:14 -0400

    [Feat] add launch log

--- src/main/scala/elbing/Application.scala
+++ src/main/scala/elbing/Application.scala
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory
 
 object Application extends App {
   val logger = LoggerFactory.getLogger(classOf[Application.type])
+  logger.info("begin elbing:")
   val config = ConfigFactory.load().getConfig("app")
   val appName = config.getString("name")
   ActorSystem(RootActor(), appName)
